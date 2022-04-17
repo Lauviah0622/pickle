585ebe06eae9bb96e5988749eceaec71107bfbb0
Lavi Fang <lauviah0622@gmail.com>
2022-04-17 23:35:47 +0800

    change prettier tabWidth

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  trailingComma: 'es5',
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true,
-  jsxSingleQuote: false,
-  printWidth: 80,
-  arrowParens: 'avoid',
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    arrowParens: 'avoid',
 }
