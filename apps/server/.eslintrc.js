1b2556c7859462c4159bf845a8dd02c20a3f77ce
Lavi Fang <lauviah0622@gmail.com>
2022-04-18 22:56:52 +0800

    set eslint

--- apps/server/.eslintrc.js
+++ apps/server/.eslintrc.js
@@ -3,26 +3,27 @@ const { join } = require('path')
 console.log('server lint ', [__dirname, join(__dirname, '../../')])
 
 module.exports = {
-  ...require('@pickle/config/eslint-node.js'),
-  parserOptions: {
-    root: true,
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'import/no-extraneous-dependencies': [
-      0, // this rule make error lint in vscode extension
-      {
-        packageDir: [__dirname, join(__dirname, '../../')],
-      },
+    ...require('@pickle/config/eslint-node.js'),
+    parserOptions: {
+        root: true,
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'import/no-extraneous-dependencies': [
+            0, // this rule make error lint in vscode extension
+            {
+                packageDir: [__dirname, join(__dirname, '../../')],
+            },
+        ],
+        'no-restricted-exports': 'off',
+    },
+    ignorePatterns: [
+        '.eslintrc.js',
+        '*.svg',
+        '*.css',
+        'jest.config.js, ./scripts/**/.js',
     ],
-  },
-  ignorePatterns: [
-    '.eslintrc.js',
-    '*.svg',
-    '*.css',
-    'jest.config.js, ./scripts/**/.js',
-  ],
 }
--- packages/config/eslint-node.js
+++ packages/config/eslint-node.js
@@ -1,77 +1,84 @@
 module.exports = {
-  env: {
-    browser: false,
-    node: true,
-    es6: true,
-  },
-  extends: ['airbnb', 'prettier'],
-  plugins: ['import'],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js'],
-      },
+    env: {
+        browser: false,
+        node: true,
+        es6: true,
     },
-  },
-  rules: {
-    'import/no-extraneous-dependencies': ['off', { devDependencies: true }],
-    'comma-dangle': [0, 'only-multiline'],
-    'import/prefer-default-export': [0],
-  },
-  overrides: [
-    {
-      // 3) Now we enable eslint-plugin-testing-library rules or preset only for matching files!
-      env: {
-        jest: true,
-      },
-      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-      extends: ['plugin:testing-library/react', 'plugin:jest/recommended'],
-      rules: {
-        'import/no-extraneous-dependencies': [
-          'off',
-          { devDependencies: ['**/?(*.)+(spec|test).[jt]s?(x)'] },
-        ],
-      },
-    },
-    {
-      files: ['*.ts'], // Your TypeScript files extension
-
-      // As mentioned in the comments, you should extend TypeScript plugins here,
-      // instead of extending them outside the `overrides`.
-      // If you don't want to extend any rules, you don't need an `extends` attribute.
-      extends: [
-        'airbnb',
-        'airbnb-typescript',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-        'prettier',
-      ],
-
-      plugins: ['@typescript-eslint', 'import'],
-
-      settings: {
-        'import/parsers': {
-          '@typescript-eslint/parser': ['.ts', '.tsx'],
-        },
+    extends: ['airbnb', 'prettier'],
+    plugins: ['import'],
+    settings: {
         'import/resolver': {
-          typescript: {
-            alwaysTryTypes: true,
-            project: ['apps/*/tsconfig.json'],
-          },
-          node: {
-            extensions: ['.js'],
-            paths: ['scripts'],
-          },
+            node: {
+                extensions: ['.js'],
+            },
         },
-      },
     },
-  ],
-  ignorePatterns: [
-    'node_modules',
-    'public',
-    'styles',
-    'coverage',
-    'dist',
-    '.turbo',
-  ],
+    rules: {
+        'import/no-extraneous-dependencies': ['off', { devDependencies: true }],
+        'comma-dangle': [0, 'only-multiline'],
+        'import/prefer-default-export': [0],
+        'no-restricted-exports': [0],
+    },
+    overrides: [
+        {
+            // 3) Now we enable eslint-plugin-testing-library rules or preset only for matching files!
+            env: {
+                jest: true,
+            },
+            files: [
+                '**/__tests__/**/*.[jt]s?(x)',
+                '**/?(*.)+(spec|test).[jt]s?(x)',
+            ],
+            extends: [
+                'plugin:testing-library/react',
+                'plugin:jest/recommended',
+            ],
+            rules: {
+                'import/no-extraneous-dependencies': [
+                    'off',
+                    { devDependencies: ['**/?(*.)+(spec|test).[jt]s?(x)'] },
+                ],
+            },
+        },
+        {
+            files: ['*.ts'], // Your TypeScript files extension
+
+            // As mentioned in the comments, you should extend TypeScript plugins here,
+            // instead of extending them outside the `overrides`.
+            // If you don't want to extend any rules, you don't need an `extends` attribute.
+            extends: [
+                'airbnb',
+                'airbnb-typescript',
+                'plugin:import/recommended',
+                'plugin:import/typescript',
+                'prettier',
+            ],
+
+            plugins: ['@typescript-eslint', 'import'],
+
+            settings: {
+                'import/parsers': {
+                    '@typescript-eslint/parser': ['.ts', '.tsx'],
+                },
+                'import/resolver': {
+                    typescript: {
+                        alwaysTryTypes: true,
+                        project: ['apps/*/tsconfig.json'],
+                    },
+                    node: {
+                        extensions: ['.js'],
+                        paths: ['scripts'],
+                    },
+                },
+            },
+        },
+    ],
+    ignorePatterns: [
+        'node_modules',
+        'public',
+        'styles',
+        'coverage',
+        'dist',
+        '.turbo',
+    ],
 }
